331de8086789a9fa1b8a064665f6f78534e747bb
Khuong Huynh <huynhkhuong69@yahoo.com>
2021-10-10 19:54:06 -0700

    Question 4

--- triple.py
+++ triple.py
@@ -1,8 +1,10 @@
+#decorator function
 def tripler(func):
 
 """The decorate_func() will pass the message "This is Python!" into the func 
 as an argument, then  the func() were used 3 times in the inner_fuc
 """
+# func() is used 3 times
 	def inner_func():
 		print("Decorate Message!")
 		func()
@@ -10,8 +12,10 @@ as an argument, then  the func() were used 3 times in the inner_fuc
 		func()
 	return inner_func
 
+#define another function
 def decorate_func():
 	print("This is Python!")
 
+# pass the decorate_func as an argument
 decorated = triple(decorate_func)
 decorated()
