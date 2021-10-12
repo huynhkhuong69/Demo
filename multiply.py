0f3c72f75b3381ab684f82c843e406089e64ddb4
Khuong Huynh <huynhkhuong69@yahoo.com>
2021-10-12 11:52:28 -0700

    Q1

---
+++ multiply.py
@@ -0,0 +1,13 @@
+import error
+
+def multiply_list(input_list):
+    try:
+        output = 1
+        for number in input_list:
+            output = output * number
+        return output
+    except error:
+        return False
+
+if __name__ == "__main__":
+    print(multiply_list([1,2,3,4,5]))
