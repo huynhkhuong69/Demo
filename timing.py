3e8526983de81bd53d0db35f7e3bc6a2824a2876
huynhkhuong69 <71426543+huynhkhuong69@users.noreply.github.com>
2021-10-12 00:24:13 -0700

    Update timing.py

--- timing.py
+++ timing.py
@@ -1,10 +1,7 @@
 import time
 
 def calculate_time():
- """The function calculate_time() will
-    compute the time to run a function.
-    Use time.time() to get time in seconds.
- """
+ """The function calculate_time() willcompute the time to run a function.Use time.time() to get time in seconds."""
 	start_time = time.time()
 	time.sleep(2)
 	end_time = time.time()
