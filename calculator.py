83df4723c61310240a1bff82d6363118244d7a04
huynhkhuong69 <71426543+huynhkhuong69@users.noreply.github.com>
2021-10-12 00:18:36 -0700

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,15 +1,23 @@
-import error
+ from os import error
 
-def calculator(number1, number2, operator):
- """This function operations will compute addition (+), 
-    subtraction(-), multiplication(*), division(/), integer division (//) and power(**) operation.
-    Function should take three parameters in this order: number1, number2, operator
-    Calculator function should return the result of the operation but if the operation was invalid return False
-    The function called parse_input(), which will take input from the user and call the calculator function
-    The parse_input() function job should take input from user, parse input from user,
-    and pass user inpt to calculator() function
+def multiply_list(input_list):
+ """This function operations will compute addition (+), subtraction(-), multiplication(*), division(/), 
+ integer division (//) and power(**) operation.
+ Function should take three parameters in this order: number1, number2, operator 
+ Calculator function should return the result of the operation
+ but if the operation was invalid return False The function called parse_input(), 
+ which will take input from the user and call the calculator function
+ The parse_input() function job should take input from user, parse input from user, and pass user inpt to calculator() function 
  """
-
+    try:
+        output = 1
+        for number in input_list:
+            output = output * number
+        return output
+    except error:
+        return False
+    
+def calculator(number1, number2, operator):
     try:
         if operator == "+":
             return number1 + number2
@@ -33,4 +41,4 @@ def parse_input():
     first_number, operator, second_number = expr.split()
     first_number = float(first_number)
     second_number = float(second_number)
-    print(calculator(first_number, second_number, operator))
+    print(calculator(first_number,second_number,operator))
