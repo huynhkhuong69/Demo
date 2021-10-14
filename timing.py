1f3a7c621d1647a929f5037a9ca2b0bbe0352d14
huynhkhuong69 <71426543+huynhkhuong69@users.noreply.github.com>
2021-10-13 17:11:42 -0700

    Update timing.py

--- timing.py
+++ timing.py
@@ -1,16 +1,33 @@
 import time
 
-def calculate_time():
+#decorator function
+def calculate_time(func):
+    """The decorator will calculate the the time will run the function"""
+    #inner function can use the outer function
+    def wrapper():
 
-    #store start time
-    start = time.time()
+        #store start time
+        #time.time() to get the current time in seconds
+        start = time.time()
+
+        #calling timer function in the wrapper function
+        timer()
+
+        #store end time
+        end = time.time()
+
+        #print total time
+        print("Total time ", end - start)
+ 
+    return wrapper
+
+#this function will be call in the wrapper function
+#use time.sleep(2) to test
+def timer():
     time.sleep(2)
 
-    #store end time
-    end = time.time()
+#let's decorate timer function
+run_timer = calculate_time(timer)
 
-    #print total time
-    print("Total time ", end - start)
-
-calculate_time()
-
+#calling the function
+run_timer()
