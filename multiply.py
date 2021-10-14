e09744fc69d732366f332440a05568e85f41fde3
huynhkhuong69 <71426543+huynhkhuong69@users.noreply.github.com>
2021-10-13 17:37:15 -0700

    Update multiply.py

--- multiply.py
+++ multiply.py
@@ -4,7 +4,7 @@ def multiply_list(input_list):
     try:
         output = 1
         for number in input_list:
-            output = output * number
+            output = int(output * number)
         return output
     except error:
         return False
