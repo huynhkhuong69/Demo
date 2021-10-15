c6e7b5015816c6070cc1920b4ee0fb50389f0b87
huynhkhuong69 <71426543+huynhkhuong69@users.noreply.github.com>
2021-10-14 21:16:31 -0700

    Update multiply.py

--- multiply.py
+++ multiply.py
@@ -1,10 +1,8 @@
-from os import error
 def multiply_list(input_list):
-    try:
         output = 1
-        for number in input_list:
-            output = output * number
+        for x in input_list:
+            if type(x) == int or type(x) == float:
+                output = output * x
+            else:
+                return False
         return output
-    except error:
-        return False
-
